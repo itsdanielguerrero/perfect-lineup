6d3ba7763629255fc87f1276dfb169003a0682cd
itsdanielguerrero <itsdanielguerrero@gmail.com>
2019-10-02 13:51:44 -0400

    added working code to getPositionCounts(lineup)+calculateTotalSalary(lineup)

--- lineup.js
+++ lineup.js
@@ -1,9 +1,21 @@
 function calculateTotalSalary(lineup) {
-  return 0
+  //use reduce to calculate total salary
+  return lineup.reduce((totalSalary, player) => {
+    //anonymous functions to add up total salary
+    return  totalSalary + player.salary
+  }, 0)
 }
 
 function getPositionCounts(lineup) {
-  return {}
+  //use reduce to count how many player in lineup play same positions
+  return lineup.reduce((positions, player) => {
+    const {position} = player
+    //conditinal statement - checks if player position is in our table then update by 1
+    const addPosition = positions [position]||[]
+    const updatePosition = addPosition + 1
+    //update my table, use parseInt() to turn my count into a integer
+    return {...positions, [position]: parseInt(updatePosition)}
+  }, {})
 }
 
 function getGameCounts(lineup) {
